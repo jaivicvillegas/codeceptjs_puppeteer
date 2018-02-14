db6b97d3c8fa5c48e73f20f62656334cd4ae6fb8
Jaivic Villegas <jvillegas@caleidos.pe>
2018-02-14 10:33:37 -0500

    inicio de puppeteer

---
+++ 0001_sblogin_sale_test.js
@@ -0,0 +1,28 @@
+/// <reference path="./steps.d.ts" />
+Feature('Login');
+
+Scenario('Registered user should login to SB Chile and logout', (I) => {
+     I.say('I want to login to SomosBelcorp Chile');
+    I.amOnPage('/');
+    I.wait(2);
+    I.see('¡Bienvenida');
+    I.selectOption('//*[@id="ddlPais"]', 'CL');
+    I.wait(2);
+    I.fillField('CodigoUsuario','usuariopruebacl');
+    I.fillField('ClaveSecreta','1234567');
+    I.click('#btnLogin');
+     I.wait(2);
+    I.amOnPage('/Bienvenida');
+    I.wait(2);
+    I.click('//*[@id="PopShowroomVenta"]/div/a/img');
+    I.wait(2);
+    I.click('//*[@id="lnk-sup-cerrar-sesion"]');
+});
+
+Scenario('Registered user should login to SB Bolivia and logout', (I,generalPage) => {
+    generalPage.sendForm('BO','usuariopruebabo','1234567');
+});
+
+Scenario('Registered user should login to SB Perú and logout', (I,generalPage) => {
+    generalPage.sendForm('PE','usuariopruebape','1234567');
+});
